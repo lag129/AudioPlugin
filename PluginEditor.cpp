567857bd27a7eca409799141839ff83fb94a7041
Itsuki_Owada <s2212215@st.tohtech.ac.jp>
2024-04-07 21:45:35 +0900

    first commit

---
+++ PluginEditor.cpp
@@ -0,0 +1,44 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
+{
+    juce::ignoreUnused (processorRef);
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (200, 400);
+    gainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 25);
+    gainSlider.setRange(-48.0, 0.0);
+    gainSlider.setValue(-1.0);
+    gainSlider.addListener(this);
+    addAndMakeVisible(gainSlider);
+}
+
+AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+
+}
+
+void AudioPluginAudioProcessorEditor::resized()
+{
+    gainSlider.setBounds(getLocalBounds());
+}
+
+void AudioPluginAudioProcessorEditor::sliderValueChanged(juce::Slider* slider)
+{
+    if (slider == &gainSlider)
+    {
+        processorRef.rawVolume = pow(10, gainSlider.getValue() / 20);
+    }
+}
